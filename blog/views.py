190818660a5f678b5d41f951c352e9be3e7e7289
Amartyanov1974 <randomquake@yandex.ru>
2023-06-12 20:34:36 +0400

    Подгрузил авторов заранее с помощью prefetch_related('author')

--- blog/views.py
+++ blog/views.py
@@ -34,24 +34,27 @@ def serialize_tag(tag):
 
 def index(request):
 
-    #posts = Post.objects.all()
-    #popular_posts = sorted(posts, key=get_likes_count)
-    #most_popular_posts = popular_posts[-5:]  # TODO. Как это посчитать?
+    # posts = Post.objects.all()
+    # popular_posts = sorted(posts, key=get_likes_count)
+    # most_popular_posts = popular_posts[-5:]  # TODO. Как это посчитать?
 
-    #posts = Post.objects.annotate(amount_likes=Count('likes'))
-    #popular_posts = sorted(posts, key=get_likes_count)
-    #most_popular_posts = popular_posts[-5:]  # TODO. Как это посчитать?
+    # posts = Post.objects.annotate(amount_likes=Count('likes'))
+    # popular_posts = sorted(posts, key=get_likes_count)
+    # most_popular_posts = popular_posts[-5:]  # TODO. Как это посчитать?
     
-    most_popular_posts = Post.objects.annotate(amount_likes=Count('likes')).order_by('-amount_likes')[:5]
+    # most_popular_posts = Post.objects.annotate(amount_likes=Count('likes')).order_by('-amount_likes')[:5]
+
+    most_popular_posts = Post.objects.prefetch_related('author').annotate(amount_likes=Count('likes')).order_by('-amount_likes')[:5]
 
 
-
-    fresh_posts = Post.objects.order_by('published_at')
+    # fresh_posts = Post.objects.order_by('published_at')
+    
+    fresh_posts = Post.objects.prefetch_related('author').order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
 
-    #tags = Tag.objects.all()
-    #popular_tags = sorted(tags, key=get_related_posts_count)
-    #most_popular_tags = popular_tags[-5:]
+    # tags = Tag.objects.all()
+    # popular_tags = sorted(tags, key=get_related_posts_count)
+    # most_popular_tags = popular_tags[-5:]
 
     most_popular_tags = Tag.objects.annotate(amount_tags=Count('posts')).order_by('-amount_tags')[:5]
 
