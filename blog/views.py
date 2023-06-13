e50ebbab632df6fbac34c4fece4e9f37b4654bf6
Amartyanov1974 <randomquake@yandex.ru>
2023-06-13 23:03:24 +0400

    Все перевел на serialize_tag_optimized

--- blog/views.py
+++ blog/views.py
@@ -52,7 +52,7 @@ def index(request):
 
 
 def post_detail(request, slug):
-    post = Post.objects.get(slug=slug)
+    post = Post.objects.prefetch_related('author').prefetch_tags().get(slug=slug)
     comments = Comment.objects.prefetch_related('author').filter(post=post)
     serialized_comments = []
     for comment in comments:
@@ -75,7 +75,7 @@ def post_detail(request, slug):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in related_tags],
+        'tags': [serialize_tag_optimized(tag) for tag in related_tags],
     }
 
     most_popular_tags = Tag.objects.popular()[:5]
