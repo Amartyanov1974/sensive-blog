082ec7a4548bac187229fc1d61d32eda19bbe6e4
Amartyanov1974 <randomquake@yandex.ru>
2023-06-15 00:07:57 +0400

    Использовал related_name

--- blog/views.py
+++ blog/views.py
@@ -46,7 +46,7 @@ def index(request):
 
 def post_detail(request, slug):
     post = get_object_or_404(Post.objects.prefetch_tags(), slug=slug)
-    comments = Comment.objects.select_related('author').filter(post=post)
+    comments = post.comments.select_related('author')
     serialized_comments = []
     for comment in comments:
         serialized_comments.append({
